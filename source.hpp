b1dfd8edd41018eaea0bc91d604128c4f88e0545
Yoav Gilad <yoavgilad1995g@gmail.com>
2023-05-22 20:10:09 +0300

    SOMETIHNG

---
+++ source.hpp
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+class Node {
+private:
+    string value;
+public:
+    Node(string text): value(text){};
+};
+
+class NUM : public Node
+{
+public:
+    NUM(string text):
+};
