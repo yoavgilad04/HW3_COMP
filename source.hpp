22acf029e065e3ca2055935d64903f0187f1c6ad
Shaked Silverman <shaked.s@cscomp.cs.technion.ac.il>
2023-05-26 14:37:21 +0300

    new classes

--- source.hpp
+++ source.hpp
@@ -11,32 +11,69 @@ using namespace std;
 class Node {
 public:
     string value;
-    Node(string text): value(text){
+    Node(string text=""): value(text){
         cout << "I'm inside node builder "<< this->value << endl;
     };
 };
 
 #define YYSTYPE Node*
 
+
+
 class Exp : public Node
 {
 public:
     string type;
-    Exp(string text, string type): Node(text), type(type){
+    Exp(string text, string type): Node(text), type(type)
+    {
         cout << "text: " << this->value << " type: " << this->type << endl;
-    };
-    Exp(Exp& exp_1, Exp& exp_2)
-};
+    }
 
-class Add : public Exp
-{
-    int true_val;
-    Add(Exp exp_1, Exp exp_2): Exp(exp_1.value, exp_1->type){
-        if(exp_1.type == 'int' && exp_1->type == exp_2->type) {
-            output::errorMismatch(yylineno);
+    Exp(Exp& exp_1, string , string conversion_type)
+    {
+
+    }
+
+    Exp(string text, string type, string operation_type): Node(text)
+    {
+
+    }
+
+    Exp(Exp& exp_1, Exp& exp_2, string operation_val)
+    {
+        if (operation_val == "+" || operation_val == "-" || operation_val == "*" || operation_val == "/")
+        {
+            if ((exp_1.type == "int" || exp_1.type == "byte") && (exp_2.type == "int" || exp_2.type == "byte"))
+            {
+                if(exp_1.type == "int" || exp_2.type == "int")
+                    this->type = "int";
+                else
+                    this->type = "byte";
+
+                switch(operation_val){
+                    case "+":
+                        this->value = exp_1.value + exp_2.value;
+                        return;
+                    case "-":
+                        this->value = exp_1.value - exp_2.value;
+                        return;
+                    case "*":
+                        this->value = exp_1.value * exp_2.value;
+                        return;
+                    case "/":
+                        this->value = exp_1.value / exp_2.value;
+                        return;
+                }
+            }
+            else
+            {
+                output::errorMismatch(yylineno);
+            }
+            return;
         }
-        cout << "num:" << this->true_val << "string" << this->value << endl;
-    };
+
+    }
+
 };
 
 #endif
