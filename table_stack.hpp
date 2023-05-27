c2c642a5631c5d56671b9cc85cb27e731d368b5f
Yoav Gilad <yoavgilad1995g@gmail.com>
2023-05-27 13:29:04 +0300

    D

---
+++ table_stack.hpp
@@ -0,0 +1,28 @@
+//
+// Created by yoavgilad on 5/27/23.
+//
+
+#ifndef HW3_TABLE_STACK_H
+#define HW3_TABLE_STACK_H
+#include <string>
+#include <iostream>
+#include "symbol_table.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+class TableStack
+{
+    std::vector<SymbolTable*> tables;
+public:
+    TableStack():tables(){}
+    void openNewScope();
+    void closeScope();
+    void addSymbolToLastTable(string name, string type);
+    Symbol* searchForSymbol(string name);
+    ~TableStack();
+};
+
+
+
+#endif //HW3_TABLE_STACK_H
